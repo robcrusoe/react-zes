8db7bbdbcbad624ff8a8ee6ef13c3a06da65f24f
Arka Sain <arka.sain@aol.com>
2021-05-20 23:55:59 +0200

    Uses the Cart Context in a component

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,9 +1,18 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext } from 'react';
 
 import CartIcon from './../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
+import CartContext from './../../store/cart-context';
+
 const HeaderCartButton = props => {
+    /* The `HeaderCartButton` component is re-evaluated everytime the CartContext changes */
+    const cartCtx = useContext(CartContext);
+
+    const numberOfCartItems = cartCtx.items.reduce((currNumber, item) => {
+        return currNumber + item.amount;
+    }, 0);
+
     return (
         <Fragment>
             <button className={classes.button} onClick={props.onShowCart}>
@@ -11,7 +20,7 @@ const HeaderCartButton = props => {
                     <CartIcon />
                 </span>
                 <span>Your Cart</span>
-                <span className={classes.badge}>3</span>
+                <span className={classes.badge}>{numberOfCartItems}</span>
             </button>
         </Fragment>
     );
