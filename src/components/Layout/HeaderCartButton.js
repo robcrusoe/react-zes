d41338a179c2ad0e58420fd0446f51a4f39c6f2f
Arka Sain <arka.sain@aol.com>
2021-05-22 18:51:38 +0200

    Makes use of the `useEffect()` hook

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useContext } from 'react';
+import React, { Fragment, useContext, useEffect, useState } from 'react';
 
 import CartIcon from './../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
@@ -6,8 +6,32 @@ import classes from './HeaderCartButton.module.css';
 import CartContext from './../../store/cart-context';
 
 const HeaderCartButton = props => {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
     /* The `HeaderCartButton` component is re-evaluated everytime the CartContext changes */
     const cartCtx = useContext(CartContext);
+
+    const { items } = cartCtx;
+
+    /* Animation for `Cart` button */
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+
+        setBtnIsHighlighted(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300);
+
+        return () => {
+            /* Clean-up function */
+            clearTimeout(timer);
+        };
+    }, [items]);
 
     const numberOfCartItems = cartCtx.items.reduce((currNumber, item) => {
         return currNumber + item.amount;
@@ -15,7 +39,7 @@ const HeaderCartButton = props => {
 
     return (
         <Fragment>
-            <button className={classes.button} onClick={props.onShowCart}>
+            <button className={btnClasses} onClick={props.onShowCart}>
                 <span className={classes.icon}>
                     <CartIcon />
                 </span>
